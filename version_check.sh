8126812561f680ba632132bafcfd988ba3660228
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-23 11:41:12 +0530

    Test Commit

--- version_check.sh
+++ version_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-sudo curl -sSL https://d2eqe25y35e72h.cloudfront.net | HYS_VERSION=0.9.5.4  bash
+sudo sudo usermod -a -G docker runner
+curl -sSL https://d2eqe25y35e72h.cloudfront.net | HYS_VERSION=0.9.5.4  bash
 #if [ "$(hyscale --version| grep  Version)" == *"$project_version"* ];
 if grep -qw "${{ steps.publish_artifacts.outputs.RELEASE_VERSION }}" <<< "$(hyscale --version| grep  Version)" ;
 
