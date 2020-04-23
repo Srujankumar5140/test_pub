4fa4713e67ebec18c4b446efde5e542e2a332f2f
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-23 11:48:34 +0530

    Test Commit

--- version_check.sh
+++ version_check.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 sudo sudo usermod -a -G docker runner
 curl -sSL https://d2eqe25y35e72h.cloudfront.net | HYS_VERSION=0.9.5.4  bash
 #if [ "$(hyscale --version| grep  Version)" == *"$project_version"* ];
-echo ${{ steps.publish_artifacts.outputs.RELEASE_VERSION }}
-if grep -qw "${{ steps.publish_artifacts.outputs.RELEASE_VERSION }}" <<< "$(hyscale --version| grep  Version)" ;
+echo ${{ steps.release_info.outputs.RELEASE_VERSION }}
+if grep -qw "${{ steps.release_info.RELEASE_VERSION }}" <<< "$(hyscale --version| grep  Version)" ;
 
 then 
 	echo "successful"
