c3bb2c9ff9f256b6cd36d4140115367fca406f65
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-23 09:01:01 +0530

    Updating Publish artifacts script to make it work according to github actions

--- publish_artifacts.sh
+++ publish_artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 project_version=`cat pom.xml | grep "<version>.*</version>" | head -1 |awk -F'[><]' '{print $3}'`
-echo ::set-env name=RELEASE_VERSION::$(echo $project_version)
+echo ::set-output name=RELEASE_VERSION::$(echo -e "${project_version}")
+#echo ::set-env name=RELEASE_VERSION::$(echo $project_version)
 artifactory_version=$project_version.$GITHUB_RUN_NUMBER
 echo ::set-env name=IMAGE_VERSION::$(echo $artifactory_version)
