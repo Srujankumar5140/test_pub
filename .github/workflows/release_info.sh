abe81c84ce85cdc130f02f3bf4e17457f30efdc8
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-28 13:53:50 +0530

    test build on file push

--- .github/workflows/release_info.sh
+++ .github/workflows/release_info.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # Only look to the latest release to determine the previous tag -- this allows us to skip unsupported tag formats (like `version-1.0.0`)
-#export CHANGELOG=`git log $PREVIOUS_TAG...$NEW_TAG --oneline`
-export CHANGELOG=`git log v0.9.254...v0.9.253 --oneline`
+export CHANGELOG=`git log $PREVIOUS_TAG...$NEW_TAG --oneline`
+#export CHANGELOG=`git log v0.9.254...v0.9.253 --oneline`
 echo "CHANGELOG=$CHANGELOG"
 
 #Format the changelog so it's markdown compatible
