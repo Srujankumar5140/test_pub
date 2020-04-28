bcd00abd7f47e4a2be760a2339d900ba35b4718d
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-28 14:04:54 +0530

    test build on file push

--- .github/workflows/release_info.sh
+++ .github/workflows/release_info.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-git fetch --prune --unshallow
 # Only look to the latest release to determine the previous tag -- this allows us to skip unsupported tag formats (like `version-1.0.0`)
 #export CHANGELOG=`git log $PREVIOUS_TAG...$NEW_TAG --oneline`
 export CHANGELOG=`git log v0.9.301...v0.9.299 --oneline`
